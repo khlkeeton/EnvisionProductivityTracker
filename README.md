771cff9efb4d205217f7a61dd61c8eee813439e8
khlkeeton <khlkeeton@gmail.com>
2023-04-13 18:19:01 -0500

    reformat for readability

--- README.md
+++ README.md
@@ -1,2 +1,15 @@
 # EnvisionProductivityTracker
-# EnvisionProdTracker
+# How To Use
+When opening, a popup window will appear. Input the parameters into the window.
+
+The right side of the window is a task bar. At the top of the bar is the productivity reminder. If it is blue, the team is on track.
+If it is yellow, the team is a little bit behind. If the bar is red, the team is behind.
+Right below, there is a button to change the parameters on a similar screen to the beginning popup.
+If a blank is not filled, the parameter will not change.
+Below that is a speaker. If people need to hear the progress of the current task outside of the refresh, 
+a message will be played.
+At the bottom is a button to close the window.
+
+An excel file is formatted in the "y-m-d_productivity_blister_pack_numWorkers_workers.xlsx"
+format.
+
--- main.py
+++ main.py
@@ -1,15 +1,15 @@
 import time
 import tkinter as tk
-from tkinter import ttk
-from tkinter import RIGHT
 from datetime import date
+
 import matplotlib.pyplot as plt
-from gtts import gTTS
-from playsound import playsound
 import numpy as np
 import pandas as pd
+from gtts import gTTS
 from matplotlib.backends.backend_tkagg import FigureCanvasTkAgg
+from playsound import playsound
 
+# set global variables used everywhere
 goal = 0
 numPerCycle = 0
 numWorker = 0
@@ -21,8 +21,8 @@ keepgoing = True
 numPerRefresh = 5
 start_time = time.time()  # to be compared with curr time in loop
 xCoord = [0]  # things used to set up graph
-yCoord = [0]
-actualYCoord = [0]
+yCoord = [0]  # ideal rate
+actualYCoord = [0]  # uses the arudino code
 timeDone = 0  # track number of items made
 timeLapsed = 0
 timeComplete = 60 * 60 * 8
@@ -33,6 +33,26 @@ def time_convert():  # can be used to show time elapsed. May be used later
 
 
 def changeParamBut():
+    def inputstuff():
+        # change global variables. If an input is not changed, do not change it
+        global goal
+        global numPerCycle
+        global numWorker
+        global refreshRate
+        global numPerRefresh
+        if len(inputToDo.get(1.0, "end-1c")) != 0:
+            goal = inputToDo.get(1.0, "end-1c")
+        if len(inputNumCycle.get(1.0, "end-1c")) != 0:
+            numPerCycle = inputNumCycle.get(1.0, "end-1c")
+        if len(inputNumWorker.get(1.0, "end-1c")) != 0:
+            numWorker = inputNumWorker.get(1.0, "end-1c")
+        if len(inputRefreshRate.get(1.0, "end-1c")) != 0:
+            refreshRate = int(inputRefreshRate.get(1.0, "end-1c")) * 60
+        numPerRefresh = (int(goal) - int(yCoord[len(yCoord) - 1])) / (
+                timeComplete - (timeLapsed - timeDone)) * refreshRate
+        top.destroy()
+
+    # create widgets needed for input parameter box
     top = tk.Toplevel(window)
     top.title("Set goals")
     top.attributes('-topmost', True)
@@ -60,26 +80,7 @@ def changeParamBut():
     numWorkerLabel.pack(side=tk.LEFT)
     refreshRateLabel = tk.Label(frame4SubWind, text="The refresh rate on this station in minutes is: ")
     refreshRateLabel.pack(side=tk.LEFT)
-
-    def inputstuff():
-        global goal
-        global numPerCycle
-        global numWorker
-        global refreshRate
-        global numPerRefresh
-        if len(inputToDo.get(1.0, "end-1c")) != 0:
-            goal = inputToDo.get(1.0, "end-1c")
-        if len(inputNumCycle.get(1.0, "end-1c")) != 0:
-            numPerCycle = inputNumCycle.get(1.0, "end-1c")
-        if len(inputNumWorker.get(1.0, "end-1c")) != 0:
-            numWorker = inputNumWorker.get(1.0, "end-1c")
-        if len(inputRefreshRate.get(1.0, "end-1c")) != 0:
-            refreshRate = int(inputRefreshRate.get(1.0, "end-1c")) * 60
-        numPerRefresh = (int(goal) - int(yCoord[len(yCoord) - 1])) / (
-                    timeComplete - (timeLapsed - timeDone)) * refreshRate
-        top.destroy()
-
-    button = tk.Button(top, text="input parameters", command=inputstuff)
+    button = tk.Button(top, text="input parameters", command=inputstuff())
     inputToDo.pack()
     inputNumCycle.pack()
     inputNumWorker.pack()
@@ -89,38 +90,7 @@ def changeParamBut():
     top.geometry("750x250")
 
 
-window = tk.Tk()
-window.attributes('-fullscreen', True)
-frame1 = tk.Frame(master=window, width=250, height=100, bg="red")
-frame1.pack(fill=tk.BOTH, side=tk.RIGHT, expand=True)
-frame2 = tk.Frame(master=window, width=100, bg="YELLOW")
-frame2.pack(fill=tk.BOTH, side=tk.BOTTOM, expand=True)
-frame3 = tk.Frame(master=window, width=50, bg="blue")
-frame3.pack(fill=tk.BOTH, side=tk.TOP, expand=True)
-frame4 = tk.Frame(master=window, width=100, bg="BLUE")
-frame4.pack(fill=tk.BOTH, side=tk.BOTTOM, expand=True)
-frame5 = tk.Frame(master=window, width=50, bg="yellow")
-frame5.pack(fill=tk.BOTH, side=tk.BOTTOM, expand=True)
-
-fig = plt.Figure(dpi=100, facecolor="white", edgecolor='white', figsize=(frame1.winfo_x(), frame1.winfo_y()))
-prodGraph = fig.add_subplot(111)
-prodGraph.xaxis.label.set_color('black')  # setting up X-axis label color to yellow
-prodGraph.yaxis.label.set_color('black')  # setting up Y-axis label color to blue
-prodGraph.tick_params(axis='x', colors='black')  # setting up X-axis tick color to red
-prodGraph.tick_params(axis='y', colors='black')  # setting up Y-axis tick color to black
-prodGraph.plot(xCoord, yCoord)
-prodGraph.set_xlabel("time")
-prodGraph.set_ylabel("cycles completed")
-prodGraph.set_title("Productivity graph")
-prodGraph.grid()
-prodGraph.margins(x=0)
-prodGraph.margins(y=0)
-canvas = FigureCanvasTkAgg(fig, frame1)
-
-buttonChangeParam = tk.Button(frame5, text="Change parameters", command=lambda: changeParamBut())
-buttonChangeParam.pack()
-
-
+# close big full screen window. Saves an extra copy of the excel spreadsheet
 def killBut():
     # creates table
     convCoord = []  # sets up array in proper format for table
@@ -132,7 +102,7 @@ def killBut():
     tableCoords = np.array(convCoord)  # convert fake table to real table
     df = pd.DataFrame(tableCoords, columns=['Time', 'numDone'])
     print(df)  # check table looks good
-    print(type(df))
+    print(type(df))  # for debugging, show table
     df.to_excel(
         '{0}-{1}-{2}_productivity_blister_pack_{3}_workers.xlsx'.format(year, month, day, numWorker))  # push to excel
     window.destroy()
@@ -141,14 +111,8 @@ def killBut():
     keepgoing = False
 
 
-buttonKill = tk.Button(frame2, text="close window",
-                       command=lambda:
-                       killBut())
-buttonKill.pack()
-
-
+# speak. Depending on parameters, the message changes
 def speakBut():
-    message = ""
     if actualYCoord[len(actualYCoord) - 1] >= yCoord[len(yCoord) - 1]:
         message = "You are on track"
     elif actualYCoord[len(actualYCoord) - 1] >= yCoord[len(yCoord) - 1] * .9:
@@ -162,23 +126,17 @@ def speakBut():
     playsound('msg.mp3')
 
 
-buttonSpeak = tk.Button(frame4, text="speak", command=lambda: speakBut())
-buttonSpeak.pack()
-
-canvas.get_tk_widget().pack(expand=True, fill=tk.BOTH)
-canvas.draw()
-
-
+# main method that loops. Repeats once a millisecond
 def task():
     global timeDone
     global timeLapsed
     timeLapsed = time.time() - start_time
     if timeLapsed % int(refreshRate) <= .1:  # checks if interval is hit
-        print(timeLapsed)  # temp proof system works
         timeDone += numPerRefresh
-        xCoord.append(timeLapsed)  # add entry to table
+        xCoord.append(int(timeLapsed / 60))  # add entry to table
         yCoord.append(timeDone)
         prodGraph.plot(xCoord, yCoord, 'g')
+        prodGraph.plot(xCoord, actualYCoord, 'b')
         canvas.draw()
         convCoord = []  # sets up array in proper format for table
         i = 0
@@ -194,12 +152,53 @@ def task():
             '{0}-{1}-{2}_productivity_blister_pack_{3}_workers.xlsx'.format(year, month, day,
                                                                             numWorker))  # push to excel
         speakBut()
-        while timeLapsed % int(refreshRate) < 1:  # ensure loop not re-entered immediately
+        while timeLapsed % int(refreshRate) < .1:  # ensure loop not re-entered immediately
             timeLapsed = time.time() - start_time
 
     window.update()
     window.after(1, lambda: task())
 
+
+window = tk.Tk()  # create window and frames for buttons
+window.attributes('-fullscreen', True)
+frame1 = tk.Frame(master=window, width=250, height=100, bg="red")
+frame1.pack(fill=tk.BOTH, side=tk.RIGHT, expand=True)
+frame2 = tk.Frame(master=window, width=100, bg="YELLOW")
+frame2.pack(fill=tk.BOTH, side=tk.BOTTOM, expand=True)
+frame3 = tk.Frame(master=window, width=50, bg="blue")
+frame3.pack(fill=tk.BOTH, side=tk.TOP, expand=True)
+frame4 = tk.Frame(master=window, width=100, bg="BLUE")
+frame4.pack(fill=tk.BOTH, side=tk.BOTTOM, expand=True)
+frame5 = tk.Frame(master=window, width=50, bg="yellow")
+frame5.pack(fill=tk.BOTH, side=tk.BOTTOM, expand=True)
+
+# graph
+fig = plt.Figure(dpi=100, facecolor="white", edgecolor='white', figsize=(frame1.winfo_x(), frame1.winfo_y()))
+prodGraph = fig.add_subplot(111)
+prodGraph.xaxis.label.set_color('black')  # setting up X-axis label color to black
+prodGraph.yaxis.label.set_color('black')  # setting up Y-axis label color to black
+prodGraph.tick_params(axis='x', colors='black')  # setting up X-axis tick color to black
+prodGraph.tick_params(axis='y', colors='black')  # setting up Y-axis tick color to black
+prodGraph.plot(xCoord, yCoord)
+prodGraph.set_xlabel("time")
+prodGraph.set_ylabel("cycles completed")
+prodGraph.set_title("Productivity graph")
+prodGraph.grid()
+prodGraph.margins(x=0)
+prodGraph.margins(y=0)
+canvas = FigureCanvasTkAgg(fig, frame1)
+canvas.get_tk_widget().pack(expand=True, fill=tk.BOTH)
+canvas.draw()
+
+# create buttons
+buttonChangeParam = tk.Button(frame5, text="Change parameters", command=lambda: changeParamBut())
+buttonChangeParam.pack()
+buttonKill = tk.Button(frame2, text="close window",
+                       command=lambda:
+                       killBut())
+buttonKill.pack()
+buttonSpeak = tk.Button(frame4, text="speak", command=lambda: speakBut())
+buttonSpeak.pack()
 
 window.after(1, func=task())
 changeParamBut()
